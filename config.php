d1d5a7f3b11f1ddd4a6db9e8111192bfd3fb2398
alain17-web <alain@MBP-de-Alain.lan>
2020-07-04 16:13:50 +0200

    ajout config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST","localhost");
+define("DB_PORT","8889");
+define("DB_NAME","portfolio");
+define("DB_LOGIN","root");
+define("DB_PWD","root");
+define("DB_CHARSET","utf8");
